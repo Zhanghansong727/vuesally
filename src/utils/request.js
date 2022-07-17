8ffa5249e546f77ff05d83af1e765c611fadcd2d
zhanghan <Zhanghansong727@163.com>
2022-07-17 19:52:53 +0800

    feat(add): 跨域、axios、api、localStorage二次封装

---
+++ src/utils/request.js
@@ -0,0 +1,45 @@
+// 引入axios
+import axios from 'axios'
+// 引入store
+// import store from '../store'
+
+const instance = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 8000
+})
+// 请求拦截器
+instance.interceptors.request.use(
+  function (config) {
+    // const token = store.getters.manager.token
+    // if (token) {
+    //   config.headers.Authorization = 'Bearer ' + token
+    // }
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
+)
+
+// 响应拦截器
+instance.interceptors.response.use(
+  function (response) {
+    // 全局响应处理
+    // if (response.data.code === 200) {
+    //   return response.data.data
+    // }
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
+)
+// 封装 处理get请求方式的参数问题
+function request(options) {
+  options.method = options.method || 'get'
+  if (options.method.toLowerCase() === 'get') {
+    options.params = options.data
+  }
+  return instance(options)
+}
+
+export default request
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,32 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  //关闭ESlint代码质量检测
+  lintOnSave: true,
+  productionSourceMap: false,
+  //配置服务器
+  devServer: {
+    //主机名
+    host: 'localhost',
+    //启动端口号
+    port: 1123,
+    //是否开启https
+    https: false,
+    //浏览器自动打开
+    open: true,
+    //配置跨域
+    proxy: {
+      //代理名称
+      [process.env.VUE_APP_BASE_API]: {
+        //target后面要写的是跨域的地址
+        target: process.env.VUE_APP_SERVICE_URL,
+        //开启跨域
+        changeOrigin: true,
+        //代理路径重写
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: ''
+        }
+      }
+    }
+  }
 })
